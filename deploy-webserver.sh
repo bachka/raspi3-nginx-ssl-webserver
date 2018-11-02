4acb3a52bfad90cd42aec3c1d15f51c5410db497
bachka <ichinnorovb@gmail.com>
2018-11-02 17:37:43 +0100

    improving some settings

--- deploy-webserver.sh
+++ deploy-webserver.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-
 cd ~
 sudo apt-get update
 sudo apt-get -y install unzip git python3.5.*
@@ -18,3 +17,29 @@ mkdir -p /etc/letsencrypt/.well-known
 cp ssl.nginx /etc/nginx/snippets/ssl.conf
 cp cors-headers.nginx /etc/nginx/snippets/cors-headers.conf
 cp routes.nginx /etc/nginx/conf.d/default.conf
+
+#certbot -d bachka.de \
+#    -d *.bachka.de \
+#    --email ichinnorovb@gmail.com \
+#    --text --agree-tos --server https://acme-v02.api.letsencrypt.org/directory - manual - priority-challenges dns \
+#    --expand --renew-by-default \
+#    --manual-public-ip-logging-ok certonly
+#
+#certbot certonly \
+#    --manual \
+#    -d *.bachka.de \
+#    -d bachka.de \
+#    --preferred-challenges dns-01 \
+#    --server https://acme-v02.api.letsencrypt.org/directory
+#
+#
+## removing unused ssl certs
+## made a backup first
+#sudo cp /etc/letsencrypt/ /etc/letsencrypt.backup -r
+## deleted the ‘no longer needed domains’
+#sudo rm -rf /etc/letsencrypt/live/bachka.de
+#sudo rm -rf /etc/letsencrypt/renewal/bachka.de.conf
+#sudo rm -rf /etc/letsencrypt/archive/bachka.de
+#sudo rm -rf /etc/letsencrypt/live/bachka.de-0001
+#sudo rm -rf /etc/letsencrypt/renewal/bachka.de-0001.conf
+#sudo rm -rf /etc/letsencrypt/archive/bachka.de-0001
